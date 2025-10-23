4bd5c3bc6440547e5ad89657138260b95da9ba6a
Hani☠️ <hanija.lc240073@gmail.com>
2025-10-23 12:43:15 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -15,7 +15,7 @@ const app = express();
 
 // Middleware
 app.use(cors({
-  origin: "http://localhost:3000", // Adjust for your frontend
+  origin: "https://subscription-lc.netlify.app", // Adjust for your frontend
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization"],
 }));
